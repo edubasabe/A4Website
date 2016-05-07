0aac204972b709ee2ebd5f8353016bf92ce843a9
Edu <edubasabe1@gmail.com>
2016-05-07 00:27:37 -0430

    Pre muestra para la asesoria
    Material para mostrar para la asesoría del día sábado 7 de mayo

---
+++ header.php
@@ -0,0 +1,51 @@
+<!-- Titulo para indexar -->
+<h1 class="invisible">A4</h1>
+<h2 class="invisible">Impresos</h2>
+<!-- Navegacion   -->
+<nav class="navbar" role="navigation">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <div class="menu-burger">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </button>
+      <!-- Logo -->
+      <a href="/" class="navbar-brand">
+        <span class="invisible">A4Website</span>
+        <div class="logo">
+          <img src="img/logo/A4Logo.svg" width="auto" height="80px" alt="Logo">
+        </div>
+      </a>
+    </div>
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+      <ul class="nav navbar-nav">
+        <span><a href="#" class="lista-precios">Lista de Precios</a></span>
+        <li><a href="#" id="myBtn">Iniciar sesión</a></li>
+        <li><a href="#">Regístrate</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container -->
+  </nav>
+
+<div class="bg-productos">
+  <div class="container">
+    <div class="row">
+    <div class="col-sm-12 col-md-12 col-lg-12">
+    <ul class="menu-productos">
+      <li><h4>Servicios</h4></li>
+      <li><a href="#">Tarjetas de presentación</a></li>
+      <li><a href="#">Volantes</a></li>
+      <li><a href="#">Pendondes</a></li>
+      <li><a href="#">Factureros</a></li>
+      <li><a href="#">Calendarios</a></li>
+      <li><a href="#">Cuarto de pliego</a></li>
+      <li><hr></li>
+    </ul>
+    </div>
+    </div>
+  </div>
+</div>
--- index.php
+++ index.php
@@ -18,106 +18,30 @@
         <link rel="stylesheet" href="css/normalize.css">
         <script src="js/vendor/modernizr-2.8.3.min.js"></script>
     </head>
-    <body>
-    <!-- preloader   -->
-    <div id="preloader" class="invisible">
-      <div id="status"><img src="img/loader-04.png" class="img-loading" width="125" height="125" alt="loading"></div>
-    </div>
-        <!-- Esto va para un header.php -->
-        <header>
-        <!-- Titulo para indexar -->
-        <h1 class="invisible">A4</h1>
-        <h2 class="invisible">Impresos</h2>
-        <!-- Navegacion   -->
-        <nav class="navbar" role="navigation">
-          <div class="container">
-            <div class="navbar-header">
-              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                <div class="menu-burger">
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                </div>
-              </button>
-              <!-- Logo -->
-              <a href="/" class="navbar-brand">
-                <span class="invisible">A4Website</span>
-                <div class="logo">
-                  <img src="img/logo/A4Logo.svg" width="auto" height="80px" alt="Logo">
-                </div>
-              </a>
-            </div>
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-              <ul class="nav navbar-nav">
-                <li><span><a href="#" class="lista-precios">Lista de Precios</a></span><br>
-                  <a href="#" id="myBtn">Iniciar sesión</a></li>
-                <li><a href="#">Regístrate</a></li>
-              </ul>
-            </div><!-- /.navbar-collapse -->
-          </div><!-- /.container -->
-          </nav>
-
-        <div class="bg-productos">
-          <div class="container">
-            <div class="row">
-            <div class="twelve columns">
-            <ul class="menu-productos">
-              <li><h4>Servicios</h4></li>
-              <li><a href="#">Tarjetas de presentación</a></li>
-              <li><a href="#">Volantes</a></li>
-              <li><a href="#">Pendondes</a></li>
-              <li><a href="#">Factureros</a></li>
-              <li><a href="#">Calendarios</a></li>
-              <li><a href="#">Cuarto de pliego</a></li>
-              <li><hr></li>
-            </ul>
-            </div>
-            </div>
-          </div>
-        </div>
-        </header>
-
-<!-- Ventana Modal Iniciar sesion -->
-<div id="myModal" class="modal">
-  <!-- Modal content -->
-  <div class="modal-content">
-    <div class="modal-header">
-      <span class="close">
-        <svg width="30" height="30">
-	        <line x1="0" y1="0" x2="30" y2="30" style="stroke:rgb(200,200,200);stroke-width:1"></line>
-          <line x1="0" y1="30" x2="30" y2="0" style="stroke:rgb(200,200,200);stroke-width:1"></line>
-        </svg>
-      </span>
-    </div>
-    <div class="modal-body">
-      <h4>Inicio de Sesión</h4>
-      <form name="inicia-sesion" action="sesion.php" method="post">
-        <div class="row">
-          <div class="twelve columns">
-            <label for="usuario">Usuario</label>
-            <input type="text" name="txtusuario" maxlength="12" placeholder="Ingrese su nombre de usuario" value="">
-            <br>
-            <label for="password">Contraseña</label>
-            <input type='password' name='txtclave' maxlength='12' placeholder='Ingrese su contraseña' id='txtclave' value=''>
-            <br>
-            <input type='submit' name='BtnEnviar' id='BtnEnviar' class='button-primary' value='Iniciar Sesión'>
-       </form>
-            <a href="#" style="margin-top:1rem;">¿Ha olvidado su usuario o contraseña?</a>
-            <hr>
-            <button class="button-secondary"><a href="#">Registrarse</a></button>
-          </div>
-        </div>
-
-    </div>
-    <div class="modal-footer"></div>
-  </div>
+<body>
+<!-- preloader   -->
+<div id="preloader" class="invisible">
+<div id="status"><img src="img/loader-04.png" class="img-loading" width="125" height="125" alt="loading"></div>
 </div>
+<header>
+<?php include_once("header.php") ?>
+</header>
+
+<?php include_once("iniciar-sesion.php") ?>
 
 <!-- Contenido del Sitio -->
+<!-- div class="jumbotron">
+  <div class="container">
+    <h1>Hello, world!</h1>
+    <p>Ahora no es necesario que vengas a nuestras oficinas para entregar tus archivos. Regístrate y súbelos aquí</p>
+    <p><a class="btn btn-primary btn-lg" href="#" role="button">Comienza Ahora</a></p>
+  </div>
+</div -->
+
 <section class="banner">
   <div class="container">
     <div class="row">
-      <div class="twelve columns">
+      <div class="col-sm-12 col-md-12 col-lg-12">
           <h2>¡Envíanos tu arte!</h2>
           <h4>Ahora no es necesario que vengas a nuestras oficinas para entregar tus archivos. Regístrate y súbelos aquí</h4>
           <button class="button-primary"><a href="" >Comienza Ahora</a></button>
@@ -132,21 +56,21 @@
       <h3>Servicios</h3>
     </div>
     <div class="row">
-      <div class="four columns">
+      <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
         <div class="box-servicio">
           <img src="img/servicios/tdp.jpg" alt="Servicios" class="img-responsive img-servicio">
           <h4>Tarjetas de Presentación</h4>
           <p>Imprime tus tarjetas de visita para todos tus potenciales clientes en las cantidades que necesites, tenemos diferentes modelos de muestra por una o ambas caras con o sin laminado.</p>
         </div>
       </div>
-      <div class="four columns">
+      <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
         <div class="box-servicio">
           <img src="img/servicios/flyers.jpg" alt="Servicios" class="img-responsive img-servicio">
           <h4>Volantes / Flyers</h4>
           <p>Justo lo que necesitas para promocionar tus productos, impresión digital Offset de volantes, flyers, tripticos, dipticos con la información de tu empresa con precios accesibles a todas las empresas.</p>
         </div>
       </div>
-      <div class="four columns">
+      <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
         <div class="box-servicio">
           <img src="img/servicios/pendones.jpg" alt="Servicios" class="img-responsive img-servicio">
           <h4>Pendones</h4>
@@ -154,29 +78,31 @@
         </div>
       </div>
     </div>
+
     <!-- Servicios 2da Fila -->
     <div class="row">
-      <div class="four columns">
+      <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
         <div class="box-servicio">
           <img src="img/servicios/factureros.jpg" alt="Servicios" class="img-responsive img-servicio">
           <h4>Factureros</h4>
           <p>Impresión de hojas de factura o factureros en lotes, a full color, monocromático o escala de grises. Para que puedas brindarles a tus clientes un soporte de venta de tus productos o servicios.</p>
         </div>
       </div>
-      <div class="four columns">
+      <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
         <div class="box-servicio">
           <img src="img/servicios/tab.jpg" alt="Servicios" class="img-responsive img-servicio">
           <h4>Impresión Individual</h4>
           <p>También tenemos la capacidad de brindarte cosas simples, como las impresiones individuales de un formato específico. Para que puedas realizar de forma personalizada todo lo que esté al alcance de tus manos.</p>
         </div>
       </div>
-      <div class="four columns">
+      <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
         <div class="box-servicio">
           <img src="img/servicios/calendarios.jpg" alt="Servicios" class="img-responsive img-servicio">
           <h4>Calendarios</h4>
           <p>Traemos para ti modelos impactantes de calendarios o almanaques que imprimimos con la mejor calidad digital Offset en tiempos record, y con acabados finales de laminado mate o brillante para que perduren en el tiempo.</p>
         </div>
       </div>
+    </div>
     </div><!-- Fin de 2da fila -->
  </div> <!-- Fin de container -->
 </section>
@@ -186,8 +112,8 @@
 <section class="quienes-somos">
       <div class="container">
         <div class="row">
-        <div class="six columns"></div>
-        <div class="six columns">
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6"></div>
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
           <h3>Quiénes somos</h3>
           <p>Somos un equipo de profesionales con más de 30 años de experiencia en la industria gráfica, que tiene como principal objetivo producir impresos de calidad. Hacemos hincapié en la calidad de nuestros trabajos, teniendo como principal objetivo la atención personalizada para satisfacción de nuestros clientes.</p>
           <p>Imprimimos tanto para ambientes de exteriores como interiores y la resolución de nuestros equipos le brindará una calidad óptima de impresión. Si su necesidad es impactar visualmente a los consumidores, el gran formato es la solución. En A4 contamos con equipos de gran formato donde podrá imprimir pendones, afiches, banners, fotos a gran tamaño.</p>
@@ -196,44 +122,11 @@
     </div>
 </section>
 
-<section class="contacto">
-
-</section>
+<section class="contacto"></section>
 
 
 <footer>
-<div class="footer">
-<div class="container">
-  <div class="row">
-    <div class="four columns">
-      <h5>A4</h5>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa est maxime numquam reprehenderit aliquam deleniti. Fuga error asperiores corrupti nam id inventore amet magnam deserunt ratione cupiditate, itaque quo, alias.</p>
-    </div>
-    <div class="four columns">
-      <h5>Menú</h5>
-      <ul class="menu-productos-footer">
-        <li><a href="#">Tarjetas de presentación</a></li>
-        <li><a href="#">Volantes</a></li>
-        <li><a href="#">Pendondes</a></li>
-        <li><a href="#">Factureros</a></li>
-        <li><a href="#">Calendarios</a></li>
-        <li><a href="#">Cuarto de pliego</a></li>
-      </ul>
-    </div>
-    <div class="four columns">
-      <h5>Redes Sociales</h5>
-      <p></p>
-    </div>
-  </div>
-</div>
-</div>
-<div class="underfoot">
-  <div class="container">
-    <div class="row">
-      <p>Copyrigth. Todos los derechos reservados.</p>
-  </div>
-  </div>
-</div>
+<?php include_once("footer.php") ?>
 </footer>
 <!-- <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script> -->
 <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
